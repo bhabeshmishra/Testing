fd3198de317970f0663583fae05ba9be8adc6870
Abhi <Abhi@LAPTOP-HT9ABSF6>
2021-10-14 21:19:29 +0530

    Updated activity 2

--- Project1/src/test/java/testing/Activity2.java
+++ Project1/src/test/java/testing/Activity2.java
@@ -1,12 +1,21 @@
 package testing;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Activity2 {
 	
 	@Test
-	public void test() {
-		System.out.println("Git Test");
+	public void test() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.quit();
 	}
 
 }
